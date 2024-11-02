bea6d412b08b61c93f060fcb4c438c64cec8db5c
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-11-02 21:33:25 +0100

    first commit

---
+++ src/main/java/com/qatorze/attimino/AttiminoApplication.java
@@ -0,0 +1,13 @@
+package com.qatorze.attimino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttiminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttiminoApplication.class, args);
+	}
+
+}
